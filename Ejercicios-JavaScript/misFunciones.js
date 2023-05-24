66b14b3c5c9c2cf15c59a8d26966c4ec84a4ef1d
arnonahmias <2222270@ucc.edu.ar>
2023-05-24 08:27:55 -0300

    js primera vez

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,30 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function conversorUnidades(id, valor) {
+    if (isNaN(valor)){
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        alert("El valor ingresado es incorrecto");
+    }
+    else if (id == "metro") {
+        document.lasUnidades.unid_yarda.value = 1.09361 * valor;
+        document.lasUnidades.unid_pie.value = 3.28084*valor;
+        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
+    }
+    else if (id == "pulgada") {
+        document.lasUnidades.unid_metro.value = valor * 0.0254;
+        document.lasUnidades.unid_pie.value = valor * 0.08333;
+        document.lasUnidades.unid_yarda.value = valor * 0.027778;
+    } else if (id == "pie") {
+        document.lasUnidades.unid_metro.value = valor * 0.3048;
+        document.lasUnidades.unid_pulgada.value = valor * 12;
+        document.lasUnidades.unid_yarda.value = valor * 0.333333;
+    } else if (id == "yarda") {
+        document.lasUnidades.unid_metro.value = valor * 0.9144;
+        document.lasUnidades.unid_pulgada.value = valor * 36;
+        document.lasUnidades.unid_pie.value = valor * 3;
+    }
+}
