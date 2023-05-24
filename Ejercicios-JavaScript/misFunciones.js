3e701b22bde9a90500d1eff869e60cba18ec5e17
arnonahmias <2222270@ucc.edu.ar>
2023-05-24 09:12:41 -0300

    ej docu

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,9 +1,9 @@
 /**
- * Descripción
+ * Conversion de unidddes, de metros, yardas, pies y pulgadas.
  * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * @param {string} id - El id de los imputs de metros, yardas, pies o pulgadas
+ * @param {number} valor - El valor de los imputs de metros, yardas, pies o pulgadas
+ * @return
  */
 function conversorUnidades(id, valor) {
     if (isNaN(valor)){
