77844aca7bc19e2d2205d82075d505559d0259cf
arnonahmias <2222270@ucc.edu.ar>
2023-05-28 21:20:50 -0300

    primera y segunda pagina

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,7 +6,7 @@
  * @return
  */
 function conversorUnidades(id, valor) {
-    if(valor.includes(",")){
+    if (valor.includes(",")) {
         valor = valor.replace(",", ".");
     }
     if (isNaN(valor)) {
@@ -16,30 +16,30 @@ function conversorUnidades(id, valor) {
         document.lasUnidades.unid_pulgada.value = "";
         alert("El valor ingresado es incorrecto");
     } else if (id == "metro") {
-        document.lasUnidades.unid_yarda.value = Math.round(1.09361 * valor * 100)/100;
-        document.lasUnidades.unid_pie.value = Math.round(3.28084 * valor * 100)/100;
-        document.lasUnidades.unid_pulgada.value = Math.round(39.3701 * valor * 100)/100;
+        document.lasUnidades.unid_yarda.value = Math.round(1.09361 * valor * 100) / 100;
+        document.lasUnidades.unid_pie.value = Math.round(3.28084 * valor * 100) / 100;
+        document.lasUnidades.unid_pulgada.value = Math.round(39.3701 * valor * 100) / 100;
     } else if (id == "pulgada") {
-        document.lasUnidades.unid_metro.value = Math.round(valor * 0.0254 * 100)/100;
-        document.lasUnidades.unid_pie.value = Math.round(valor * 0.08333 * 100)/100;
-        document.lasUnidades.unid_yarda.value = Math.round(valor * 0.027778 * 100)/100;
+        document.lasUnidades.unid_metro.value = Math.round(valor * 0.0254 * 100) / 100;
+        document.lasUnidades.unid_pie.value = Math.round(valor * 0.08333 * 100) / 100;
+        document.lasUnidades.unid_yarda.value = Math.round(valor * 0.027778 * 100) / 100;
     } else if (id == "pie") {
-        document.lasUnidades.unid_metro.value = Math.round(valor * 0.3048)/100;
-        document.lasUnidades.unid_pulgada.value = Math.round(valor * 12 * 100)/100;
-        document.lasUnidades.unid_yarda.value = Math.round(valor * 0.333333 * 100)/100;
+        document.lasUnidades.unid_metro.value = Math.round(valor * 0.3048) / 100;
+        document.lasUnidades.unid_pulgada.value = Math.round(valor * 12 * 100) / 100;
+        document.lasUnidades.unid_yarda.value = Math.round(valor * 0.333333 * 100) / 100;
     } else if (id == "yarda") {
-        document.lasUnidades.unid_metro.value = Math.round(valor * 0.9144 * 100)/100;
-        document.lasUnidades.unid_pulgada.value =  Math.round(valor * 36 * 100)/100;
-        document.lasUnidades.unid_pie.value =  Math.round(valor * 3 * 100)/100;
+        document.lasUnidades.unid_metro.value = Math.round(valor * 0.9144 * 100) / 100;
+        document.lasUnidades.unid_pulgada.value = Math.round(valor * 36 * 100) / 100;
+        document.lasUnidades.unid_pie.value = Math.round(valor * 3 * 100) / 100;
     }
 }
 
 function convertirGR(id) {
     var grad, rad;
-    if (id == "grados"){
+    if (id == "grados") {
         grad = document.getElementById("grados").value;
         rad = (grad * Math.PI) / 180;
-    } else if (id == "radianes"){
+    } else if (id == "radianes") {
         rad = document.getElementById("radianes").value;
         grad = (rad * 180) / Math.PI;
     }
@@ -49,38 +49,60 @@ function convertirGR(id) {
     document.getElementById("radianes").value = rad;
 }
 
-function mostrar_ocultar(valorMo){
-    if(valorMo== "val_mostrar"){
+function mostrar_ocultar(valorMo) {
+    if (valorMo == "val_mostrar") {
         document.getElementById("divMo").style.display = 'block';
-    }else if(valorMo=="val_ocultar"){
+    } else if (valorMo == "val_ocultar") {
         document.getElementById("divMo").style.display = 'none';
     }
 }
 
-function calcularSuma(){
+function calcularSuma() {
     var num1, num2;
-    num1=document.getElementsByName("sum_num1") [0].value;
-    num2=document.getElementsByName("sum_num2") [0].value;
-    document.getElementsByName("sum_total") [0].innerHTML=Number(num1) + Number(num2);
+    num1 = document.getElementsByName("sum_num1") [0].value;
+    num2 = document.getElementsByName("sum_num2") [0].value;
+    document.getElementsByName("sum_total") [0].innerHTML = Number(num1) + Number(num2);
 }
 
-function calcularResta(){
+function calcularResta() {
     var num1, num2;
-    num1=document.getElementsByName("res_num1") [0].value;
-    num2=document.getElementsByName("res_num2") [0].value;
-    document.getElementsByName("res_total") [0].innerHTML=Number(num1) - Number(num2);
+    num1 = document.getElementsByName("res_num1") [0].value;
+    num2 = document.getElementsByName("res_num2") [0].value;
+    document.getElementsByName("res_total") [0].innerHTML = Number(num1) - Number(num2);
 }
 
-function calcularMultiplicacion(){
+function calcularMultiplicacion() {
     var num1, num2;
-    num1=document.getElementsByName("mul_num1") [0].value;
-    num2=document.getElementsByName("mul_num2") [0].value;
-    document.getElementsByName("mul_total") [0].innerHTML=Number(num1) * Number(num2);
+    num1 = document.getElementsByName("mul_num1") [0].value;
+    num2 = document.getElementsByName("mul_num2") [0].value;
+    document.getElementsByName("mul_total") [0].innerHTML = Number(num1) * Number(num2);
 }
 
-function calcularDivision(){
+function calcularDivision() {
     var num1, num2;
-    num1=document.getElementsByName("div_num1") [0].value;
-    num2=document.getElementsByName("div_num2") [0].value;
-    document.getElementsByName("div_total") [0].innerHTML=Number(num1) / Number(num2);
+    num1 = document.getElementsByName("div_num1") [0].value;
+    num2 = document.getElementsByName("div_num2") [0].value;
+    document.getElementsByName("div_total") [0].innerHTML = Number(num1) / Number(num2);
+}
+
+function cargarweb() {
+    var cant, unidad, urlComp;
+
+    cant = document.getElementById("distancia").value;
+    unidad = document.getElementsByName("unidades")[0].value;
+
+    urlComp = "segundaWeb.html#" + cant + "#" + unidad;
+    window.open(urlComp);
+
+}
+
+function cargarRresultado() {
+    var urlComp, can, un;
+
+    urlComp = window.location.href.split("/")[5];
+
+    can = urlComp.split("#")[1];
+    un = urlComp.split("#")[2];
+
+    document.getElementById("dist").value = can + " " + un;
 }
